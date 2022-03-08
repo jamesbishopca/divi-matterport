c37441288a8dad16a2582f5509f299c65a820fd1
James Bishop <james@jamesbishop.ca>
2022-03-08 13:11:47 -0400

    Initial commit from Create React App

---
+++ includes/__PluginName.php
@@ -0,0 +1,46 @@
+<?php
+
+class DIMA_DiviMatterport extends DiviExtension {
+
+	/**
+	 * The gettext domain for the extension's translations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $gettext_domain = 'dima-divi-matterport';
+
+	/**
+	 * The extension's WP Plugin name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $name = 'divi-matterport';
+
+	/**
+	 * The extension's version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * DIMA_DiviMatterport constructor.
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public function __construct( $name = 'divi-matterport', $args = array() ) {
+		$this->plugin_dir     = plugin_dir_path( __FILE__ );
+		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+
+		parent::__construct( $name, $args );
+	}
+}
+
+new DIMA_DiviMatterport;
